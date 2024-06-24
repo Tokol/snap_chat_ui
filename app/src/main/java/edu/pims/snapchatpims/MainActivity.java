50ba3cfb06774f63fc50b226372cdaccc8cdf848
suresh lama <sureshlama@sureshs-MacBook-Air.local>
2024-06-24 08:42:55 +0545

    2nd class of linear layout

--- app/src/main/java/edu/pims/snapchatpims/MainActivity.java
+++ app/src/main/java/edu/pims/snapchatpims/MainActivity.java
@@ -1,14 +1,37 @@
 package edu.pims.snapchatpims;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imageView;
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
        setContentView(R.layout.activity_main);
+        imageView = (ImageView) findViewById(R.id.logoImg);
+
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //code here//
+/// on clicked is triggered here
+                Log.d("testing", "clicked");
+
+
+            }
+        });
+
+
 
 
     }

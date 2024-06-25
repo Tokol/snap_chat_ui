c085da2f7222d91759eea02efdc19938e82beb0b
suresh lama <sureshlama@sureshs-MacBook-Air.local>
2024-06-25 08:36:25 +0545

    3rd class chapter 3

--- app/src/main/java/edu/pims/snapchatpims/MainActivity.java
+++ app/src/main/java/edu/pims/snapchatpims/MainActivity.java
@@ -1,4 +1,5 @@
 package edu.pims.snapchatpims;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -7,10 +8,13 @@ import android.widget.ImageView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.material.chip.Chip;
+
 public class MainActivity extends AppCompatActivity {
 
+    // Java variable  Initlize
     ImageView imageView;
-
+    Chip btnLogin, btnSingUp;
 
 
     @Override
@@ -18,18 +22,40 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
        setContentView(R.layout.activity_main);
+       //assocating java to xml ids;
+
         imageView = (ImageView) findViewById(R.id.logoImg);
 
+        btnLogin = (Chip) findViewById(R.id.loginBtn);
+        btnSingUp= (Chip)findViewById(R.id.signUpBtn);
+
+        //clickable event
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 //code here//
 /// on clicked is triggered here
                 Log.d("testing", "clicked");
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            //we will call intent class
+
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+
+                startActivity(intent);
+
 
 
             }
         });
+
+
+
 
 
 
